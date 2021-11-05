c3b13e02e351e387030d86233793783d5771ec3d
江天文 <symfund@gmail.com>
2021-11-06 02:10:39 +0800

    refactoring offline build

--- setup_ma35d1.sh
+++ setup_ma35d1.sh
@@ -163,12 +163,19 @@ fi
 cd ~/Projects/yocto
 until repo sync --force-sync 
 do
-  echo -e "${GREEN}repo sync failed, retry... ${NC}"
+  echo -e "${RED}repo sync failed, retry... ${NC}"
+
+  if [[ -f ~/Projects/yocto/sources ]]; then
+    echo -e "${GREEN}The script detects that repo has completed the synchronization ever, skips sync this time.${NC}" 
+    break
+  fi
 done
 
 # default distro & machine
 distro=nvt-ma35d1-directfb
 machine=ma35d1-evb
+
+# If exists the local build configuration file local.conf, read distro and machine from that
 
 echo -e "${YELLOW}Select which board to build ... i: IoT e: EVB s: SOM ${NC}" 
 echo -e "${GREEN}Type 'i', 'e', or 's' ... default: e (EVB board selected)${NC}"
@@ -210,53 +217,45 @@ fi
 
 offline_build() {
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_arm-trusted-firmware-v2.3.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-bsp/tf-a/tf-a-ma35d1_2.3.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "master"/' ${YP}/sources/meta-ma35d1/recipes-bsp/tf-a/tf-a-ma35d1_2.3.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-bsp/tf-a/tf-a-ma35d1_2.3.bb
+  declare -A ASSOC_ARRAY_REPO_RECP 
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_linux-5.4.y.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-kernel/linux/linux-ma35d1_5.4.110.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "master"/' ${YP}/sources/meta-ma35d1/recipes-kernel/linux/linux-ma35d1_5.4.110.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-kernel/linux/linux-ma35d1_5.4.110.bb
+  ARM_TRUSTED_FIRMWARE_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_arm-trusted-firmware-v2.3.git"
+  ARM_TRUSTED_FIRMWARE_RECP="${YP}/sources/meta-ma35d1/recipes-bsp/tf-a/tf-a-ma35d1_2.3.bb"
+  ASSOC_ARRAY_REPO_RECP[$ARM_TRUSTED_FIRMWARE_REPO]=$ARM_TRUSTED_FIRMWARE_RECP
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_NuWriter.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-devtools/python/python3-nuwriter_0.90.0.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "master"/' ${YP}/sources/meta-ma35d1/recipes-devtools/python/python3-nuwriter_0.90.0.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-devtools/python/python3-nuwriter_0.90.0.bb
+  LINUX_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_linux-5.4.y.git"
+  LINUX_RECP="${YP}/sources/meta-ma35d1/recipes-kernel/linux/linux-ma35d1_5.4.110.bb"
+  ASSOC_ARRAY_REPO_RECP[$LINUX_REPO]=$LINUX_RECP
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_optee_os-v3.9.0.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-security/optee/optee-os-ma35d1_3.9.0.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "'master'"/' ${YP}/sources/meta-ma35d1/recipes-security/optee/optee-os-ma35d1_3.9.0.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-security/optee/optee-os-ma35d1_3.9.0.bb
+  NUWRITER_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_NuWriter.git"
+  NUWRITER_RECP="${YP}/sources/meta-ma35d1/recipes-devtools/python/python3-nuwriter_0.90.0.bb"
+  ASSOC_ARRAY_REPO_RECP[$NUWRITER_REPO]=$NUWRITER_RECP 
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_RTP_BSP.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-bsp/m4proj/m4proj_0.90.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "'master'"/' ${YP}/sources/meta-ma35d1/recipes-bsp/m4proj/m4proj_0.90.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-bsp/m4proj/m4proj_0.90.bb
+  OPTEE_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_optee_os-v3.9.0.git"
+  OPTEE_RECP="${YP}/sources/meta-ma35d1/recipes-security/optee/optee-os-ma35d1_3.9.0.bb"
+  ASSOC_ARRAY_REPO_RECP[$OPTEE_REPO]=$OPTEE_RECP 
 
-  cd ${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_u-boot-v2020.07.git/
-  if [[ "$1" == "Y" ]]; then 
-    sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${YP}/sources/meta-ma35d1/recipes-bsp/u-boot/u-boot-ma35d1_2020.07.bb
-  else
-    sed -i 's/^SRCREV.*/SRCREV = "'master'"/' ${YP}/sources/meta-ma35d1/recipes-bsp/u-boot/u-boot-ma35d1_2020.07.bb
-  fi
-  #cat ${YP}/sources/meta-ma35d1/recipes-bsp/u-boot/u-boot-ma35d1_2020.07.bb
+  M4PROJ_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_RTP_BSP.git"
+  M4PROJ_RECP="${YP}/sources/meta-ma35d1/recipes-bsp/m4proj/m4proj_0.90.bb"
+  ASSOC_ARRAY_REPO_RECP[$M4PROJ_REPO]=$M4PROJ_RECP
+
+  UBOOT_REPO="${YP}/downloads/git2/github.com.OpenNuvoton.MA35D1_u-boot-v2020.07.git"
+  UBOOT_RECP="${YP}/sources/meta-ma35d1/recipes-bsp/u-boot/u-boot-ma35d1_2020.07.bb"
+  ASSOC_ARRAY_REPO_RECP[$UBOOT_REPO]=$UBOOT_RECP
+
+  for repo in "${!ASSOC_ARRAY_REPO_RECP[@]}"
+  do
+    #echo -e "${GREEN}repo: $repo${NC}"
+    #echo -e "${YELLOW}recipe: ${ASSOC_ARRAY_REPO_RECP[$repo]}${NC}"
+    cd $repo
+    if [[ "$1" == "Y" ]]; then
+      sed -i 's/^SRCREV.*/SRCREV = "'$(git rev-parse HEAD)'"/' ${ASSOC_ARRAY_REPO_RECP[$repo]}
+    else
+      sed -i 's/^SRCREV.*/SRCREV = "master"/' ${ASSOC_ARRAY_REPO_RECP[$repo]} 
+    fi
+    #cat ${ASSOC_ARRAY_REPO_RECP[$repo]}
+  done
+
 
   if grep -q "BB_NO_NETWORK" ${YP}/build/conf/local.conf; then
     #found 
